657cdf3ef834d5bc8fa1732b9c37005013770233
SergeiKulishov <78534037+SergeiKulishov@users.noreply.github.com>
2021-02-06 19:37:09 +0300

    Initial commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning " : "Good Afternoon";
+
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponce)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponce);
+
+                return View("Thanks", guestResponce);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+
+    }
+}
+
+
+
+
+
+
